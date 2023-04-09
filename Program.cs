ecb940701650ba4fefd3954ec49c2e7c7aef8303
Viktor <shelehov.viktor.25@gmail.com>
2023-04-09 22:40:37 +0600

    add Task 4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите первое число: ");
+string userInputА = Console.ReadLine();
+int userNumberA = int.Parse(userInputА);
+Console.WriteLine(userInputА);
+Console.Write("Введите второе число: ");
+string userInputB = Console.ReadLine();
+int userNumberB = int.Parse(userInputB);
+Console.WriteLine(userInputB);
+Console.Write("Введите третье число: ");
+string userInputC = Console.ReadLine();
+int userNumberC = int.Parse(userInputC);
+Console.WriteLine(userInputC);
+int max = userNumberA;
+    if(userNumberB>max)
+    {
+         max = userNumberB;
+    }
+    if(userNumberC>max)
+    {
+         max = userNumberC;
+    }
+Console.WriteLine($"{userNumberA},{userNumberB},{userNumberC} ->{max}");
