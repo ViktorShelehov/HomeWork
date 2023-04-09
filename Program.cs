c3a194de21d66f204df2fa4cb13f0f02ad77b5d5
Viktor <shelehov.viktor.25@gmail.com>
2023-04-09 22:42:58 +0600

    ...

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое целое число: ");
+string userInputА = Console.ReadLine();
+int userNumberA = int.Parse(userInputА);
+Console.WriteLine(userInputА);
+Console.Write("Введите второе целое число: ");
+string userInputB = Console.ReadLine();
+int userNumberB = int.Parse(userInputB);
+Console.WriteLine(userInputB);
+    if (userNumberA > userNumberB)
+    {
+        Console.WriteLine($"a = {userNumberA}, b = {userNumberB} -> max = {userNumberA}");
+    }
+    else
+    {
+        Console.WriteLine($"a = {userNumberA}, b = {userNumberB} -> max = {userNumberB} ");
+    }
